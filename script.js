6de2177bb6f0e8be20b8651221a53f61c04bf624
Edosomwan Uhunoma <uhunoma93@gmail.com>
2022-06-24 15:52:37 +0100

    Commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-Let my_name = "My name is Edosomwan Uhunoma Jeffery."
-Let height = "I'm 6 feet and 2 inches tall."
-Let country = "I am from Nigeria."
+let my_name = "My name is Edosomwan Uhunoma Jeffery."
+let height = "I'm 6 feet and 2 inches tall."
+let country = "I am from Nigeria."
 
 document.write('Name : '+ my_name + '<br>' +'Height : ' + height + '<br>' + 'Country : ' + country);
 
